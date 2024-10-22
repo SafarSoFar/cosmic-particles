381f2af8a75aaf060bbd9e99f331004ed0b63eb5
Isaev Safar <133248217+SafarSoFar@users.noreply.github.com>
2024-10-22 08:26:30 -0700

    added friction, velocity change

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
-import { clamp, randFloat, randInt } from 'three/src/math/MathUtils.js';
+import { clamp, normalize, randFloat, randInt } from 'three/src/math/MathUtils.js';
 import { AsciiEffect } from 'three/addons/effects/AsciiEffect.js';
 import {GUI} from 'lil-gui';
 
@@ -25,9 +25,11 @@ document.body.appendChild( renderer.domElement );
 
 
 let settings = {
-     dotsAmount: 1000,
+     dotsAmount: 100,
      dotTraversalRange: 100,
      dotsRadius: 1,
+     distanceAffection: 100,
+     frictionRate : 0.005,
 }
 
 let mousePos = new THREE.Vector3(0,0,0);
@@ -51,6 +53,9 @@ function changeDotsAmount(value){
 
 
 const gui = new GUI();
+gui.add(settings, "distanceAffection", 100, 1000, 10);
+gui.add(settings, "frictionRate", 0.005, 0.01);
+gui.add(settings, "dotsAmount", 1, 1000).onChange(value => changeDotsAmount(value));
 
 
 function changeDotsRadius(radius){
@@ -71,20 +76,64 @@ class Dot{
      constructor(geometry, material){
           this.mesh = new THREE.Mesh(geometry, material);
 
-          this.mesh.position.set(randInt(-window.innerWidth/4,window.innerWidth/4), randInt(-window.innerHeight/4,window.innerHeight/4), randInt(0,100));
+          this.mesh.position.set(randInt(-window.innerWidth/4,window.innerWidth/4), randInt(-window.innerHeight/4,window.innerHeight/4), 0);
+          this.velocityX = 0.0;
+          this.velocityY = 0.0;
+     }
+     setVelocity(){
+          let dir = new THREE.Vector2(mousePos.x-this.mesh.position.x, mousePos.y-this.mesh.position.y);
+          dir.normalize();
+          // console.log("dir x:" + dir.x);
+          // console.log("dir y:" + dir.y);
+          this.velocityX = dir.x;
+          this.velocityY = dir.y;
      }
 
      move(){
-          this.mesh.position.x = this.mesh.position.x + mousePos.normalize().x;
-          this.mesh.position.y = this.mesh.position.y + mousePos.normalize().y;// + mousePos.normalize.y * 0.001;
-          // this.mesh.position.z = this.mesh.position.z + mousePos.normalize().z;
+          this.mesh.position.x += this.velocityX;
+          this.mesh.position.y += this.velocityY; 
+          // this.mesh.position.lerp(mousePos, 0.01);
+     }
+     friction(){
+          if(this.velocityX > 0.0){
+               if(this.velocityX - settings.frictionRate < 0.0){
+                    this.velocityX = 0.0;
+               }
+               else {
+                    this.velocityX -= settings.frictionRate;
+               }
+          }
+          if(this.velocityX < 0.0){
+               if(this.velocityX + settings.frictionRate > 0.0){
+                    this.velocityX = 0.0;
+               }
+               else{
+                    this.velocityX += settings.frictionRate;
+               }
+          }
 
+          
+          if(this.velocityY > 0.0){
+               if(this.velocityY - 0.1 < 0.0){
+                    this.velocityY = 0.0;
+               }
+               else {
+                    this.velocityY -= 0.1;
+               }
+               
+          }
+          if(this.velocityY < 0.0){
+               if(this.velocityY + 0.1 > 0.0){
+                    this.velocityY = 0.0;
+               }
+               else {
+                    this.velocityY += 0.1;
+               }
+          }
+
+          
      }
-     getRandPos(){
-          return new THREE.Vector3(randInt(-settings.dotTraversalRange,settings.dotTraversalRange), 
-          randInt(-settings.dotTraversalRange,settings.dotTraversalRange), 
-          randInt(-settings.dotTraversalRange,settings.dotTraversalRange));
-     }
+     
 
 }
 
@@ -111,6 +160,10 @@ function onDocumentMouseMove(event){
 	var dir = vector.sub( camera.position ).normalize();
 	var distance = - camera.position.z / dir.z;
 	mousePos = camera.position.clone().add( dir.multiplyScalar( distance ) );
+     // console.log("X" + mousePos.x);
+     // console.log("Y:" + mousePos.y);
+     // console.log("Z:" + mousePos.z);
+
 }
 
 
@@ -124,10 +177,16 @@ camera.position.z = 300;
 
 function animate() {
      for(let i = 0; i < settings.dotsAmount; i++){
+          if(dots[i].mesh.position.distanceTo(mousePos) < settings.distanceAffection){
+               dots[i].setVelocity();
+          }
+
           dots[i].move();
+          dots[i].friction();
      }   
 
      renderer.render( scene, camera ); 
+
 
 } 
 
